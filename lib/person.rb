a4fc0d4d66baac46105b508952a7b01f443dbfa6
Jamie H Pham <jpham1109@gmail.com>
2021-02-03 16:15:09 -0500

    add changes

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
-# your code goes here
+require 'pry'
+
+class Person 
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+   
+end 
+
+person1 = Person.new("Linda")
+#binding.pry
+0
